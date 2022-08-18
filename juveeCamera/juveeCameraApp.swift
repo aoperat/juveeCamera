825126d4e6d26d92d97ad237ffe5d31201cbdceb
Jay <aoperat@naver.com>
2022-08-18 09:31:15 +0900

    Initial Commit

---
+++ juveeCamera/juveeCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  juveeCameraApp.swift
+//  juveeCamera
+//
+//  Created by 이종환 on 2022/08/18.
+//
+
+import SwiftUI
+
+@main
+struct juveeCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
